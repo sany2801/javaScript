f3362e32f09f74be0fe17c8f87f8ef082a958c8c
Sasha Granc <sasha82018201@gmail.com>
2021-11-23 19:05:52 +0300

    finished

--- lesson 3 DOM/script.js
+++ lesson 3 DOM/script.js
@@ -1,7 +1,7 @@
-// const div = document.getElementById("lesson")
-// const divByName = document.getElementsByName('dom')
-// const divClassName = document.getElementsByClassName("wrapper")
-// const divTagName = document.getElementsByTagName('div')
+// // const div = document.getElementById("lesson")
+// // const divByName = document.getElementsByName('dom')
+// // const divClassName = document.getElementsByClassName("wrapper")
+// // const divTagName = document.getElementsByTagName('div')
 // div.style.color = 'blue'
 // console.log(div)
 // console.log(divByName)
@@ -14,7 +14,14 @@
 // console.log(getElement)
 // console.log(getAllElements)
 
-const arr = [1, 2, 3]
-const arr1 = [5, 6, 7]
-const marged = [0, ...arr, 4, ...arr1]
-console.log(marged)
+let div = document.getElementsByClassName('www')[0]
+let p = [...div.querySelectorAll('p')]
+let pw = div.querySelector('p')
+
+console.log(pw)
+
+console.log(p)
+// const arr = [1, 2, 3]
+// const arr1 = [5, 6, 7]
+// const marged = [0, ...arr, 4, ...arr1]
+// console.log(marged)
