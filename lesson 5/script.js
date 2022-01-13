8000d78b769414358f53d53bb2cf6883f213e8bc
Александр <sasha82018201@gmail.com>
2022-01-14 01:54:07 +0300

    le

--- lesson 5/script.js
+++ lesson 5/script.js
@@ -1,8 +1,19 @@
-const button = document.querySelector('#button')
-const inputTittle = document.querySelector('#tittle')
-const inputDescription = document.querySelector('#discription')
+const Data = {
+    todo: [],
+    InProgres: [],
+    done: [],
+    deleted: []
+};
 
+const todoList = document.querySelector('.todo-list')
+const button = document.querySelector('#button')
+const tittle = document.querySelector('#tittle')
+const discription = document.querySelector('#discription')
 button.addEventListener('click', (event) =>{
     event.preventDefault()
-    console.log('click')
+    console.log(Data.todo)
+    Data.todo.push({
+        tittle: tittle.value,
+        discription: discription.value
+    })
 })
